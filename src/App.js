ce590be821d5588056ee5253d687468935f6741f
Berhan Serin <berhan.serin@ajansspor.com>
2023-06-08 16:29:48 +0300

    drag list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import ReactDragListView from 'react-drag-listview';
 
 function App() {
+  const [d, setd] = useState([]);
+
+  const [index, setindex] = useState(0);
+
+  useEffect(() => {
+    const data = [];
+    for (let i = 0, len = 50; i < len; i++) {
+      data.push({
+        title: `rows ${i}`,
+        order: i
+      });
+    }
+    setd(data);
+  }, []);
+
+  const dragProps = {
+    onDragEnd(fromIndex, toIndex) {
+      // const data = [...d];
+      // const item = data.splice(fromIndex, 1)[0];
+      // data.splice(toIndex, 0, item);
+      // setd(data);
+    },
+    nodeSelector: 'li',
+    handleSelector: 'a'
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={index}
+        onChange={(e) => setindex(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          let dnew = [];
+          for (let i = 0; i < 50; i += 1) {
+            let j = i;
+            if (Number(index) === i) {
+              dnew.push({ title: `rows ${i}`, order: j });
+              dnew.push({ title: `rows ${i}`, order: (j += 1) });
+            } else if (Number(index) < i) {
+              dnew.push({ title: `rows ${i}`, order: (j += 1) });
+            } else {
+              dnew.push({ title: `rows ${i}`, order: j });
+            }
+          }
+          setd(dnew);
+        }}
+      >
+        Tıkla
+      </button>
+
+      <ReactDragListView {...dragProps}>
+        <ol>
+          {d.map((item, index) => (
+            <li key={index}>
+              {item?.title} {item?.order} {item?.index}
+            </li>
+          ))}
+        </ol>
+      </ReactDragListView>
     </div>
   );
 }
